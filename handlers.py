23fd2a1a75c9aa57db44582972d73ef626ef4b24
Frank Hamand <fhamand@geneity.co.uk>
2013-05-03 17:26:44 +0100

    fix previous commit

--- handlers.py
+++ handlers.py
@@ -75,18 +75,19 @@ def get_user(handler):
 
 class RequestHandler(tornado.web.RequestHandler):
 	def error(self, msg):
-        self.clear()
-        self.set_status(400)
-        self.finish(json.dumps({
-
-        }))
+		self.clear()
+		self.set_status(400)
+		self.finish(json.dumps({
+			'type': 'error',
+			'error': msg
+		}))
 
 
 	def prepare(self):
 		self.user = get_user(self)
 		if self.user.connection is None:
 			self.set_status(400)
-			raise tornado.web.HTTPError(400, 'Not connected')
+			self.error('Not connected')
 
 
 class Create(RequestHandler):
@@ -100,10 +101,10 @@ class Create(RequestHandler):
 		game = Game(name, user, max_players)
 
 		if not (0 < max_players <= 4):
-			raise tornado.web.HTTPError(400, 'Invalid max_players')
+			self.error('Invalid max_players')
 
 		if name in games:
-			raise tornado.web.HTTPError(400, 'Game already exists')
+			self.error('Game already exists')
 
 		games[name] = game
 
@@ -115,7 +116,7 @@ class Join(RequestHandler):
 		name = self.get_argument('name')
 
 		if name not in games:
-			raise tornado.web.HTTPError(400, 'Game doesn\'t exist')
+			self.error('Game doesn\'t exist')
 
 		game = games[name]
 		game.add_player(user)
@@ -125,13 +126,13 @@ class Start(RequestHandler):
 		user = self.user
 		game = user.game
 		if game is None:
-			raise tornado.web.HTTPError(400, 'Not in game')
+			self.error('Not in game')
 
 		if game.creator is not user:
-			raise tornado.web.HTTPError(400, 'Not game owner')
+			self.error('Not game owner')
 
 		if not game.all_ready():
-			raise tornado.web.HTTPError(400, 'Players are not ready')
+			self.error('Players are not ready')
 
 		res = requests.post(
 			'{game_host}/create'.format(game_host='http://localhost:8080'),
@@ -142,7 +143,7 @@ class Start(RequestHandler):
 		)
 
 		if res.status_code != 200:
-			raise tornado.web.HTTPError(400, res.text)
+			self.error(res.text)
 
 		game_info = {
 			'name': game.name,
@@ -155,12 +156,11 @@ class Start(RequestHandler):
 				'game': game_info
 			})
 
-
 class Ready(RequestHandler):
 	def post(self):
 		user = self.user
 		if user.game is None:
-			raise tornado.web.HTTPError(400, 'Not in game')
+			self.error('Not in game')
 
 		user.ready = True
 
@@ -168,7 +168,7 @@ class Unready(RequestHandler):
 	def post(self):
 		user = self.user
 		if user.game is None:
-			raise tornado.web.HTTPError(400, 'Not in game')
+			self.error('Not in game')
 
 		user.ready = False
 
